07721fb9524b5f79ce2349f529f1e6d553993622
r3tr0boy <r3tr0boy1234@proton.me>
2025-04-03 11:33:14 -0300

    tipos de dados

---
+++ tipos_dados/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Tipos int 
+	// int8, int16, int32, int64, int
+	
+	// Tipos uint 
+	// uint8, uint16, uint32, uint64 ,uint 
+	
+	// Tipos float
+	// float32, float64 
+	
+	// String
+	var nome string = "joao"
+	fmt.Println(nome)
+	
+	// Char
+	char := 'a'
+	fmt.Println(char)
+	
+	// Booleanos
+	// true ou false
+	
+	// Erros (sim, é um tipo)
+	var erro error 
+	fmt.Println(erro) 
+}
